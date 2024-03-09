a3ab827168606ce8dee7603dde102ae5621265f0
Rifat Khan <rifat6633@gmail.com>
2024-03-09 09:29:16 +0600

    create: sign in page

---
+++ app/(auth)/auth/sign-in/page.tsx
@@ -0,0 +1,34 @@
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import Link from "next/link";
+import SignInForm from "./_parts/signin-form";
+
+export default function Page() {
+  return (
+    <main className="min-h-screen grid place-content-center">
+      <Card className="w-96">
+        <CardHeader>
+          <CardTitle>Sign in</CardTitle>
+        </CardHeader>
+
+        <CardContent>
+          <SignInForm />
+        </CardContent>
+
+        <CardFooter>
+          <p className="text-sm">
+            Don&apos;t Have an account?{" "}
+            <Link href={"/auth/sign-up"} className="font-semibold underline">
+              Create an accout
+            </Link>
+          </p>
+        </CardFooter>
+      </Card>
+    </main>
+  );
+}
