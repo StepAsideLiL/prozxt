f3145e4e37563315a977c10daae9c6b4eff79891
Rifat Khan <rifat6633@gmail.com>
2024-03-09 16:30:56 +0600

    add: loading state

--- app/(auth)/auth/sign-in/_parts/signin-form.tsx
+++ app/(auth)/auth/sign-in/_parts/signin-form.tsx
@@ -15,6 +15,8 @@ import {
 import { Input } from "@/components/ui/input";
 import { signInUser } from "./actions";
 import { toast } from "sonner";
+import { useState } from "react";
+import { RotateCw } from "lucide-react";
 
 const formSchema = z.object({
   username: z.string().min(1, {
@@ -26,6 +28,7 @@ const formSchema = z.object({
 });
 
 export default function SignInForm() {
+  const [loading, setLoading] = useState(false);
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -35,15 +38,20 @@ export default function SignInForm() {
   });
 
   async function onSubmit(values: z.infer<typeof formSchema>) {
+    setLoading(true);
+
     const res = await signInUser(values);
     if (res?.error === "Invalid username") {
       toast.error("Invalid username");
+      setLoading(false);
     }
     if (res?.error === "Invalid password") {
       toast.error("Invalid password");
+      setLoading(false);
     }
     if (res?.error === "Incorrect username or password") {
       toast.error("Incorrect username or password");
+      setLoading(false);
     }
   }
 
@@ -88,7 +96,14 @@ export default function SignInForm() {
           )}
         />
 
-        <Button type="submit">Sign In</Button>
+        {!loading ? (
+          <Button type="submit">Sign In</Button>
+        ) : (
+          <Button disabled>
+            <RotateCw className="mr-2 h-4 w-4 animate-spin" />
+            Signing in
+          </Button>
+        )}
       </form>
     </Form>
   );
