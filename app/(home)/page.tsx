5c9a891b511324155afc8f2d87a81b922084cdc3
Rifat Khan <rifat6633@gmail.com>
2024-03-09 19:22:44 +0600

    add: preview different types of fonts

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,7 +1,14 @@
+import { besley, monofett, rubikMonoOne, vt323 } from "@/lib/fonts";
+import { cn } from "@/lib/utils";
+
 export default function Page() {
   return (
     <main className="grid place-content-center py-10">
-      <h1 className="text-x">Hello, World</h1>
+      <h1 className={cn("text-3xl", monofett.className)}>Hello, World</h1>
+      <h1 className={cn("text-3xl", vt323.className)}>Hello, World</h1>
+      <h1 className={cn("text-3xl", rubikMonoOne.className)}>Hello, World</h1>
+      <h1 className={cn("text-3xl", besley.className)}>Hello, World</h1>
+      <h1 className={cn("text-3xl")}>Hello, World</h1>
     </main>
   );
 }
