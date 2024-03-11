aaad1ea835ad50a1fdaa4a6b7a74063d3159ba63
Rifat Khan <rifat6633@gmail.com>
2024-03-11 11:23:29 +0600

    change: path formate

--- app/(user-card)/card/_parts/card-content.tsx
+++ app/(user-card)/card/_parts/card-content.tsx
@@ -1,3 +1,7 @@
+import { getUserCard } from "@/lib/data/user";
+
 export default async function CardContent({ username }: { username: string }) {
+  const user = await getUserCard(username);
+
   return <div>{username} Card Content</div>;
 }
