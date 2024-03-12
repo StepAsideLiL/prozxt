c98275ea236ed62a9fe4b88e452ebe59972835e3
Rifat Khan <rifat6633@gmail.com>
2024-03-12 08:52:18 +0600

    design: card layout

--- app/(user-card)/card/_parts/card-content.tsx
+++ app/(user-card)/card/_parts/card-content.tsx
@@ -1,12 +1,71 @@
-import { getUserCard } from "@/lib/data/user";
+import { FullLogo } from "@/components/prozxt-ui/logo";
+import { getCurrentUserCard } from "@/lib/data/user";
+import { monofett, vt323 } from "@/lib/fonts";
+import { cn } from "@/lib/utils";
 import { redirect } from "next/navigation";
+import { SiGithub, SiInstagram, SiX } from "react-icons/si";
+import CardContentForm from "./card-content-form";
 
 export default async function CardContent() {
-  const user = await getUserCard();
+  const user = await getCurrentUserCard();
 
   if (!user) {
     redirect("/auth/sign-in");
   }
 
-  return <div>{user?.username} Card Content</div>;
+  return (
+    <div className="flex w-full justify-between">
+      <CardContentForm />
+
+      <div className="aspect-[96/45] w-[700px] max-w-3xl select-none rounded-2xl border p-10">
+        <div className="flex h-full w-full flex-col">
+          <div className="flex flex-1 items-start gap-5">
+            <div>
+              <div className="aspect-square w-12 rounded-full border"></div>
+            </div>
+
+            <div className="flex-1">
+              <h1 className="text-4xl font-semibold">Rifat Khan</h1>
+              <p className="text-sm text-muted-foreground">
+                Full-stack Developer
+              </p>
+            </div>
+
+            <div className="flex flex-col items-end gap-2">
+              <FullLogo size={100} />
+              <p className="text-right text-sm text-muted-foreground">
+                Portfolio and Project
+              </p>
+            </div>
+          </div>
+
+          <div className="flex items-end justify-between gap-5">
+            <div
+              className={cn(
+                "text-9xl uppercase text-muted-foreground",
+                vt323.className,
+              )}
+            >
+              {user.username}
+            </div>
+
+            <div>
+              <p className="flex items-center gap-2">
+                <SiX />
+                <span>hello</span>
+              </p>
+              <p className="flex items-center gap-2">
+                <SiGithub />
+                <span>helloascas</span>
+              </p>
+              <p className="flex items-center gap-2">
+                <SiInstagram />
+                <span>helloascasc</span>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
