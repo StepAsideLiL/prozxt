609b49f1189dfbc4ae9a38522abfe7ecb6d8546d
Rifat Khan <rifat6633@gmail.com>
2024-03-12 13:39:05 +0600

    add: socialIcons for profile links

---
+++ components/prozxt-ui/lists.tsx
@@ -0,0 +1,47 @@
+import {
+  SiFacebook,
+  SiGithub,
+  SiInstagram,
+  SiLinkedin,
+  SiThreads,
+  SiTiktok,
+  SiX,
+} from "react-icons/si";
+
+export const socialIcons = [
+  {
+    id: "x",
+    socialTitle: "X",
+    icon: <SiX />,
+  },
+  {
+    id: "github",
+    socialTitle: "GitHub",
+    icon: <SiGithub />,
+  },
+  {
+    id: "instagram",
+    socialTitle: "Instagram",
+    icon: <SiInstagram />,
+  },
+  {
+    id: "linkedin",
+    socialTitle: "Linkedin",
+    icon: <SiLinkedin />,
+  },
+  {
+    id: "facebook",
+    socialTitle: "Facebook",
+    icon: <SiFacebook />,
+  },
+  {
+    id: "threads",
+    socialTitle: "Threads",
+    icon: <SiThreads />,
+  },
+  {
+    id: "tiktok",
+    socialTitle: "Tiktok",
+    icon: <SiTiktok />,
+  },
+];
