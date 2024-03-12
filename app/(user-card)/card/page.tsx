959a3805ed1fb7a1c4728916d7d9b978f342a9a6
Rifat Khan <rifat6633@gmail.com>
2024-03-12 09:18:57 +0600

    add: CardContentForm

--- app/(user-card)/card/page.tsx
+++ app/(user-card)/card/page.tsx
@@ -1,12 +1,19 @@
 import { Suspense } from "react";
 import CardContent from "./_parts/card-content";
+import CardContentForm from "./_parts/card-content-form";
 
 export default function Page() {
   return (
-    <main className="min-h-screen grid place-content-center">
-      <Suspense fallback={"loading"}>
-        <CardContent />
-      </Suspense>
+    <main className="container">
+      <section className="flex min-h-screen w-full items-center justify-center">
+        <Suspense fallback={"loading"}>
+          <CardContent />
+        </Suspense>
+      </section>
+
+      <section id="edit-card" className="flex justify-center py-10">
+        <CardContentForm />
+      </section>
     </main>
   );
 }
