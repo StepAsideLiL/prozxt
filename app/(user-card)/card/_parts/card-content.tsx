65aa80ec2ff3f92e6720d258ed8fc4b371b61de6
Rifat Khan <rifat6633@gmail.com>
2024-03-12 09:36:30 +0600

    refactor: move CardContentForm to CardContent

--- app/(user-card)/card/_parts/card-content.tsx
+++ app/(user-card)/card/_parts/card-content.tsx
@@ -6,6 +6,7 @@ import { cn } from "@/lib/utils";
 import Link from "next/link";
 import { redirect } from "next/navigation";
 import { SiGithub, SiInstagram, SiX } from "react-icons/si";
+import CardContentForm from "./card-content-form";
 
 export default async function CardContent() {
   const user = await getCurrentUserCard();
@@ -15,62 +16,68 @@ export default async function CardContent() {
   }
 
   return (
-    <div className="space-y-3">
-      <section className="aspect-[96/45] w-[700px] select-none rounded-2xl border p-10">
-        <div className="flex h-full w-full flex-col">
-          <div className="flex flex-1 items-start gap-5">
-            <div>
-              <div className="aspect-square w-12 rounded-full border"></div>
+    <>
+      <div className="flex min-h-screen w-full flex-col items-center justify-center gap-3">
+        <section className="aspect-[96/45] w-[700px] select-none rounded-2xl border p-10">
+          <div className="flex h-full w-full flex-col">
+            <div className="flex flex-1 items-start gap-5">
+              <div>
+                <div className="aspect-square w-12 rounded-full border"></div>
+              </div>
+
+              <div className="flex-1">
+                <h1 className="text-4xl font-semibold">Rifat Khan</h1>
+                <p className="text-sm text-muted-foreground">
+                  Full-stack Developer
+                </p>
+              </div>
+
+              <div className="flex flex-col items-end gap-2">
+                <FullLogo size={100} />
+                <p className="text-right text-[10px] text-muted-foreground">
+                  Portfolio and Project
+                </p>
+              </div>
             </div>
 
-            <div className="flex-1">
-              <h1 className="text-4xl font-semibold">Rifat Khan</h1>
-              <p className="text-sm text-muted-foreground">
-                Full-stack Developer
-              </p>
-            </div>
+            <div className="flex items-end justify-between gap-5">
+              <div
+                className={cn(
+                  "text-9xl uppercase text-muted-foreground",
+                  vt323.className,
+                )}
+              >
+                {user.username}
+              </div>
 
-            <div className="flex flex-col items-end gap-2">
-              <FullLogo size={100} />
-              <p className="text-right text-[10px] text-muted-foreground">
-                Portfolio and Project
-              </p>
+              <div>
+                <p className="flex items-center gap-2">
+                  <SiX />
+                  <span>hello</span>
+                </p>
+                <p className="flex items-center gap-2">
+                  <SiGithub />
+                  <span>helloascas</span>
+                </p>
+                <p className="flex items-center gap-2">
+                  <SiInstagram />
+                  <span>helloascasc</span>
+                </p>
+              </div>
             </div>
           </div>
+        </section>
 
-          <div className="flex items-end justify-between gap-5">
-            <div
-              className={cn(
-                "text-9xl uppercase text-muted-foreground",
-                vt323.className,
-              )}
-            >
-              {user.username}
-            </div>
+        <section>
+          <Button variant={"outline"} asChild>
+            <Link href={"#edit-card"}>Edit Your Card</Link>
+          </Button>
+        </section>
+      </div>
 
-            <div>
-              <p className="flex items-center gap-2">
-                <SiX />
-                <span>hello</span>
-              </p>
-              <p className="flex items-center gap-2">
-                <SiGithub />
-                <span>helloascas</span>
-              </p>
-              <p className="flex items-center gap-2">
-                <SiInstagram />
-                <span>helloascasc</span>
-              </p>
-            </div>
-          </div>
-        </div>
+      <section id="edit-card" className="flex justify-center py-10">
+        <CardContentForm />
       </section>
-
-      <section>
-        <Button variant={"outline"} asChild>
-          <Link href={"#edit-card"}>Edit Your Card</Link>
-        </Button>
-      </section>
-    </div>
+    </>
   );
 }
--- app/(user-card)/card/page.tsx
+++ app/(user-card)/card/page.tsx
@@ -5,15 +5,9 @@ import CardContentForm from "./_parts/card-content-form";
 export default function Page() {
   return (
     <main className="container">
-      <section className="flex min-h-screen w-full items-center justify-center">
-        <Suspense fallback={"loading"}>
-          <CardContent />
-        </Suspense>
-      </section>
-
-      <section id="edit-card" className="flex justify-center py-10">
-        <CardContentForm />
-      </section>
+      <Suspense fallback={"loading"}>
+        <CardContent />
+      </Suspense>
     </main>
   );
 }
