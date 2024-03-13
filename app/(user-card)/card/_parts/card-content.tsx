05cade55c6250c70a34fc6c3ad2883345928526c
Rifat Khan <rifat6633@gmail.com>
2024-03-13 14:44:46 +0600

    remove: card to UserCard component

--- app/(user-card)/card/_parts/card-content.tsx
+++ app/(user-card)/card/_parts/card-content.tsx
@@ -8,6 +8,7 @@ import { redirect } from "next/navigation";
 import CardContentForm from "./card-content-form";
 import { CardIcons, CardSocials } from "@/lib/types";
 import { cardIcons, socialIcons } from "@/components/prozxt-ui/lists";
+import UserCard from "./user-card";
 
 export default async function CardContent() {
   const user = await getCurrentUserCard();
@@ -21,65 +22,13 @@ export default async function CardContent() {
   return (
     <>
       <div className="flex min-h-screen w-full flex-col items-center justify-center gap-3">
-        <section className="aspect-[96/45] w-[700px] select-none rounded-2xl border p-10">
-          <div className="flex h-full w-full flex-col">
-            <div className="flex flex-1 items-start gap-5">
-              <div>
-                <div className="aspect-square w-12 rounded-full border"></div>
-              </div>
-
-              <div className="flex-1">
-                <h1 className="text-4xl font-semibold">
-                  {user.card?.name || "(no name)"}
-                </h1>
-                <p className="text-sm text-muted-foreground">
-                  {user.card?.title || "(no title)"}
-                </p>
-              </div>
-
-              <div className="flex flex-col items-end gap-2">
-                <FullLogo size={100} />
-                <p className="text-right text-[10px] text-muted-foreground">
-                  Portfolio and Project
-                </p>
-              </div>
-            </div>
-
-            <div className="flex items-end justify-between gap-5">
-              <div>
-                <h1
-                  className={cn(
-                    "text-6xl uppercase text-muted-foreground",
-                    vt323.className,
-                  )}
-                >
-                  {user.username}
-                </h1>
-                <div className="flex gap-2">
-                  {icons.map((icon) => (
-                    <span key={icon.id}>
-                      {cardIcons.find((i) => i.id === icon.id)?.iconMd}
-                    </span>
-                  ))}
-                </div>
-              </div>
-
-              <div>
-                {socials.map((social) => (
-                  <Link
-                    href={`${socialIcons.find((s) => s.id === social.id)?.profileHref}${social.username}`}
-                    key={social.id}
-                    target="_blank"
-                    className="flex items-center gap-2"
-                  >
-                    {socialIcons.find((s) => s.id === social.id)?.icon}
-                    {social.username}
-                  </Link>
-                ))}
-              </div>
-            </div>
-          </div>
-        </section>
+        <UserCard
+          username={user.username}
+          name={user.card!.name}
+          title={user.card!.title}
+          socials={socials}
+          icons={icons}
+        />
 
         <section>
           <Button variant={"outline"} asChild>
