4d30953c4c38fd1bc8c20768d86de6c6b55a0fe2
Rifat Khan <rifat6633@gmail.com>
2024-03-14 15:36:47 +0600

    add: getCurrentUser function to get logged in user info

--- lib/data/user.ts
+++ lib/data/user.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import prisma from "@/lib/prismadb";
+import { validateRequest } from "@/lib/auth";
 
 export async function getUser(username: string) {
   try {
@@ -18,5 +19,31 @@ export async function getUser(username: string) {
   } catch (err) {
     console.log(err);
     throw new Error(`Failed to get ${username}.`);
+  }
+}
+
+export async function getCurrentUser() {
+  try {
+    const { user } = await validateRequest();
+
+    if (user) {
+      const currentUser = await prisma.user.findUnique({
+        where: {
+          username: user.username,
+        },
+        select: {
+          id: true,
+          username: true,
+          profilePicture: true,
+        },
+      });
+
+      return currentUser;
+    }
+
+    return null;
+  } catch (err) {
+    console.log(err);
+    throw new Error(`Failed to get current user.`);
   }
 }
