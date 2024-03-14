a8114415cf938530bd9df5e2448b133d13bc828d
Rifat Khan <rifat6633@gmail.com>
2024-03-14 14:31:18 +0600

    add: ProfilePicturePreview, ImgbbImageRes and ImgbbFormData types

--- lib/types.d.ts
+++ lib/types.d.ts
@@ -8,3 +8,54 @@ export type CardIcons = {
   id: string;
   title: string;
 }[];
+
+export type ProfilePicturePreview = {
+  name: string;
+  type: string;
+  url: string;
+};
+
+export type ImgbbImageRes = {
+  data: {
+    id: string;
+    title: string;
+    url_viewer: string;
+    url: string;
+    display_url: string;
+    width: number;
+    height: number;
+    size: number;
+    time: number;
+    expiration: number;
+    image: {
+      filename: string;
+      name: string;
+      mime: string;
+      extension: string;
+      url: string;
+    };
+    thumb: {
+      filename: string;
+      name: string;
+      mime: string;
+      extension: string;
+      url: string;
+    };
+    delete_url: string;
+  };
+  success: boolean;
+  status: number;
+};
+
+export type ImgbbFormData = {
+  imgbbId: string;
+  title: string;
+  url: string;
+  imgbbUrl: string;
+  deleteUrl: string;
+  width: number;
+  height: number;
+  mime: string;
+  size: number;
+  time: number;
+};
