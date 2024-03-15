c06bac712e1df03bd219fe2f70a2484a09c1b5ab
Rifat Khan <rifat6633@gmail.com>
2024-03-15 14:52:06 +0600

    add: getUserCard function

--- lib/data/card.ts
+++ lib/data/card.ts
@@ -27,3 +27,30 @@ export async function getCurrentUserCard() {
     throw new Error(`Failed to get ${user?.username} card info.`);
   }
 }
+
+export async function getUserCard(username: string) {
+  try {
+    const currentUser = await prisma.user.findUnique({
+      where: {
+        username: username,
+      },
+      select: {
+        id: true,
+        username: true,
+        card: true,
+        profilePicture: {
+          select: {
+            title: true,
+            mime: true,
+            url: true,
+          },
+        },
+      },
+    });
+
+    return currentUser;
+  } catch (err) {
+    console.log(err);
+    throw new Error(`Failed to get ${username} card info.`);
+  }
+}
