2740bd80376527c4c3457f40ceb726f41fad02e5
Rifat Khan <rifat6633@gmail.com>
2024-03-30 20:52:53 +0600

    add: LexicalEditor component

--- app/(add-edit)/add/post/page.tsx
+++ app/(add-edit)/add/post/page.tsx
@@ -1,3 +1,11 @@
+import LexicalEditor from "@/components/lexical-rte/lexical-editor";
+
 export default function Page() {
-  return <div>Add post</div>;
+  return (
+    <main className="container space-y-10 py-10">
+      <h1 className="text-xl font-semibold">Add Post</h1>
+
+      <LexicalEditor />
+    </main>
+  );
 }
