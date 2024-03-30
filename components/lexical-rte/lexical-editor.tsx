769d7cb94575c73f33da7855cc99d12c1b78bc2e
Rifat Khan <rifat6633@gmail.com>
2024-03-30 21:12:44 +0600

    update: simple Lexical editor wraper with initial config

--- components/lexical-rte/lexical-editor.tsx
+++ components/lexical-rte/lexical-editor.tsx
@@ -1,3 +1,26 @@
+"use client";
+
+import {
+  InitialConfigType,
+  LexicalComposer,
+} from "@lexical/react/LexicalComposer";
+import { theme } from "./theme";
+import Editor from "./editor";
+
 export default function LexicalEditor() {
-  return <div>LexicalEditor</div>;
+  const initialConfig: InitialConfigType = {
+    namespace: "Prozxt Lexical Editor",
+    theme: theme,
+    onError: (error: Error) => {
+      throw error;
+    },
+  };
+
+  return (
+    <section>
+      <LexicalComposer initialConfig={initialConfig}>
+        <Editor />
+      </LexicalComposer>
+    </section>
+  );
 }
