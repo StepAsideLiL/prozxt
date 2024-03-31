c424fe24a2ecd8d120f948bd4a07ae9eacda2235
Rifat Khan <rifat6633@gmail.com>
2024-03-31 15:03:12 +0600

    create: Nodes and add: nodes to initial lexical config

--- components/lexical-rte/lexical-editor.tsx
+++ components/lexical-rte/lexical-editor.tsx
@@ -6,11 +6,13 @@ import {
 } from "@lexical/react/LexicalComposer";
 import { theme } from "./theme";
 import Editor from "./editor";
+import { Nodes } from "./nodes";
 
 export default function LexicalEditor() {
   const initialConfig: InitialConfigType = {
     namespace: "Prozxt Lexical Editor",
     theme: theme,
+    nodes: [...Nodes],
     onError: (error: Error) => {
       throw error;
     },
---
+++ components/lexical-rte/nodes.ts
@@ -0,0 +1,14 @@
+import { Klass, LexicalNode } from "lexical";
+import { CodeNode } from "@lexical/code";
+import { LinkNode } from "@lexical/link";
+import { ListItemNode, ListNode } from "@lexical/list";
+import { HeadingNode, QuoteNode } from "@lexical/rich-text";
+
+export const Nodes: Array<Klass<LexicalNode>> = [
+  HeadingNode,
+  ListNode,
+  ListItemNode,
+  QuoteNode,
+  CodeNode,
+  LinkNode,
+];
