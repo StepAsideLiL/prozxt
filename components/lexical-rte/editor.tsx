a70691016b34ac8fbbaba7ef8fd1802a91512a52
Rifat Khan <rifat6633@gmail.com>
2024-03-31 15:03:30 +0600

    add: MarkdownPlugin

--- components/lexical-rte/editor.tsx
+++ components/lexical-rte/editor.tsx
@@ -1,6 +1,7 @@
 import { RichTextPlugin } from "@lexical/react/LexicalRichTextPlugin";
 import { ContentEditable } from "@lexical/react/LexicalContentEditable";
 import LexicalErrorBoundary from "@lexical/react/LexicalErrorBoundary";
+import MarkdownPlugin from "./plugins/MarkdownPlugin";
 
 export default function Editor() {
   return (
@@ -18,6 +19,7 @@ export default function Editor() {
         }
         ErrorBoundary={LexicalErrorBoundary}
       />
+      <MarkdownPlugin />
     </div>
   );
 }
