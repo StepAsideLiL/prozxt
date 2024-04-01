7389ff63eed2859e06a55a60e23cd12f6f485b15
Rifat Khan <rifat6633@gmail.com>
2024-04-01 19:44:06 +0600

    update: move title and text field to NewPostInput

--- app/(new-edit)/new/post/page.tsx
+++ app/(new-edit)/new/post/page.tsx
@@ -1,9 +1,9 @@
-import LexicalEditor from "@/components/lexical-rte/lexical-editor";
+import NewPostForm from "./_parts/new-post-form";
 
 export default function Page() {
   return (
     <main className="container space-y-10 py-3">
-      <LexicalEditor />
+      <NewPostForm />
     </main>
   );
 }
