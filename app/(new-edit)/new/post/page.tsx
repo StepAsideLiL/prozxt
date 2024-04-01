9ab42236a2f797129c744118202f9117979e5510
Rifat Khan <rifat6633@gmail.com>
2024-04-01 12:39:16 +0600

    remove: site title

--- app/(new-edit)/new/post/page.tsx
+++ app/(new-edit)/new/post/page.tsx
@@ -2,9 +2,7 @@ import LexicalEditor from "@/components/lexical-rte/lexical-editor";
 
 export default function Page() {
   return (
-    <main className="container space-y-10 py-10">
-      <h1 className="text-xl font-semibold">Add Post</h1>
-
+    <main className="container space-y-10 py-3">
       <LexicalEditor />
     </main>
   );
