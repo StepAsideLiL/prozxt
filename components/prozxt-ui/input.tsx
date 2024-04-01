46babe96897d4e4ef786d0b788d84823770c3eac
Rifat Khan <rifat6633@gmail.com>
2024-04-01 22:48:22 +0600

    create: TagInput input field to add tags

--- components/prozxt-ui/input.tsx
+++ components/prozxt-ui/input.tsx
@@ -22,4 +22,21 @@ const NewPostInput = React.forwardRef<HTMLInputElement, InputProps>(
 );
 NewPostInput.displayName = "NewPostInput";
 
-export { NewPostInput };
+const TagInput = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, type, ...props }, ref) => {
+    return (
+      <input
+        type={type}
+        className={cn(
+          "flex w-full bg-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50",
+          className,
+        )}
+        ref={ref}
+        {...props}
+      />
+    );
+  },
+);
+TagInput.displayName = "TagInput";
+
+export { NewPostInput, TagInput };
