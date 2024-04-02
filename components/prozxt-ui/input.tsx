9f6bbee6dda71ad1d5a9122de956921ccef0e763
Rifat Khan <rifat6633@gmail.com>
2024-04-02 09:39:57 +0600

    update: transparent background for TagInput component

--- components/prozxt-ui/input.tsx
+++ components/prozxt-ui/input.tsx
@@ -28,7 +28,7 @@ const TagInput = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-          "flex w-full bg-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50",
+          "flex w-full bg-transparent file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50",
           className,
         )}
         ref={ref}
