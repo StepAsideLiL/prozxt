019d3ef8c42edb91beaf1a0f7f440071762d0f39
Rifat Khan <rifat6633@gmail.com>
2024-04-02 09:41:27 +0600

    update: add tags field

--- app/(new-edit)/new/project/_parts/new-project-form.tsx
+++ app/(new-edit)/new/project/_parts/new-project-form.tsx
@@ -1,15 +1,22 @@
 "use client";
 
 import LexicalEditor from "@/components/lexical-rte/lexical-editor";
-import { NewPostInput } from "@/components/prozxt-ui/input";
+import { NewPostInput, TagInput } from "@/components/prozxt-ui/input";
 import { Button } from "@/components/ui/button";
 import { EditorState } from "lexical";
 import { Plus } from "lucide-react";
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 import { addNewProject } from "./actions";
+import { Badge } from "@/components/ui/badge";
+
+type FormData = {
+  tags: string;
+};
 
 export default function NewProjectForm() {
   const [title, setTitle] = useState("");
+  const [formData, setFormData] = useState<FormData>({ tags: "" });
+  const [tags, setTags] = useState<string[]>([]);
   const [editorState, setEditorState] = useState<EditorState>();
 
   function handleClick() {
@@ -18,6 +25,11 @@ export default function NewProjectForm() {
       editorState: editorState?.toJSON(),
     };
     addNewProject(data);
+  }
+
+  function hangleTagSubmit(e: FormEvent) {
+    e.preventDefault();
+    console.log(formData);
   }
 
   return (
@@ -31,6 +43,26 @@ export default function NewProjectForm() {
         <Button variant={"outline"} onClick={() => handleClick()}>
           <Plus size={16} /> Add Project
         </Button>
+      </section>
+
+      <section className="relative">
+        {tags.length !== 0 &&
+          tags.map((tag, i) => <Badge key={i}>{tag}</Badge>)}
+
+        {formData.tags === "" && tags.length === 0 && (
+          <div className="absolute inset-0 -z-20 text-muted">Tags...</div>
+        )}
+
+        <form onSubmit={(e) => hangleTagSubmit(e)}>
+          <TagInput
+            name="tags"
+            value={formData.tags}
+            onChange={(e) => setFormData({ ...formData, tags: e.target.value })}
+          />
+          <Button type="submit" className="sr-only">
+            Add Tag
+          </Button>
+        </form>
       </section>
 
       <LexicalEditor
