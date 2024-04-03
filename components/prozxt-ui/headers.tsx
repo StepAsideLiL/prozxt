104ec8471b03a5bfab82d0bd786b6005ffc023c9
Rifat Khan <rifat6633@gmail.com>
2024-04-03 23:14:24 +0600

    add: border-b

--- components/prozxt-ui/headers.tsx
+++ components/prozxt-ui/headers.tsx
@@ -6,6 +6,25 @@ import { UserProfileSidebar } from "@/components/prozxt-ui/nav-menus";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { UserAvatarSkeleton } from "@/components/prozxt-ui/skeletons";
 
+export function HomeHeader() {
+  return (
+    <div className="border-b">
+      <header className="container flex items-center justify-between py-3">
+        <div className="flex items-center gap-5">
+          <Logo size={24} variant="link" />
+
+          <h1 className="font-semibold">Home</h1>
+        </div>
+
+        <div className="flex items-center gap-5">
+          <ModeToggle />
+          <UserAvatarSidebar />
+        </div>
+      </header>
+    </div>
+  );
+}
+
 export function NewEditHeader({ title }: { title?: string }) {
   return (
     <header className="container flex items-center justify-between py-3">
