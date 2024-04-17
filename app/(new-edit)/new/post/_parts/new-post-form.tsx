8cacbc4ec73c0ee2475ab012fd44053f052772a1
Rifat Khan <rifat6633@gmail.com>
2024-04-17 22:27:53 +0600

    add: publish button loading state

--- app/(new-edit)/new/post/_parts/new-post-form.tsx
+++ app/(new-edit)/new/post/_parts/new-post-form.tsx
@@ -4,7 +4,7 @@ import LexicalEditor from "@/components/lexical-rte/lexical-editor";
 import { NewPostInput } from "@/components/prozxt-ui/input";
 import { Button } from "@/components/ui/button";
 import { EditorState } from "lexical";
-import { Plus } from "lucide-react";
+import { Plus, RotateCw } from "lucide-react";
 import { useState } from "react";
 import { addNewPost } from "./actions";
 import { toast } from "sonner";
@@ -19,9 +19,12 @@ export default function NewPostForm({
 }) {
   const [title, setTitle] = useState("");
   const [editorState, setEditorState] = useState<EditorState>();
+  const [loading, setLoading] = useState(false);
   const router = useRouter();
 
   async function handleClick() {
+    setLoading(true);
+
     const data = {
       userId,
       title,
@@ -43,21 +46,30 @@ export default function NewPostForm({
         router.push(`/${username}/posts/${res.postId}`);
       } else {
         toast.error(res.message);
+        setLoading(false);
       }
     } else {
       toast.error("Write something First!");
+      setLoading(false);
     }
   }
 
   return (
     <section className="mx-auto max-w-3xl space-y-6">
-      <Button
-        variant={"outline"}
-        onClick={() => handleClick()}
-        className="gap-1"
-      >
-        <Plus size={16} /> Publish Post
-      </Button>
+      {loading ? (
+        <Button variant={"outline"} disabled className="gap-1">
+          <RotateCw className="mr-2 h-4 w-4 animate-spin" size={16} />{" "}
+          Publishing...
+        </Button>
+      ) : (
+        <Button
+          variant={"outline"}
+          onClick={() => handleClick()}
+          className="gap-1"
+        >
+          <Plus size={16} /> Publish Post
+        </Button>
+      )}
 
       <NewPostInput
         placeholder="Post Title..."
