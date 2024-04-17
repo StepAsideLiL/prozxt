e39782975986405255f18eaa1219daa5d23e80f8
Rifat Khan <rifat6633@gmail.com>
2024-04-17 19:29:49 +0600

    update: pass username and userId prop

--- app/(new-edit)/new/project/page.tsx
+++ app/(new-edit)/new/project/page.tsx
@@ -1,9 +1,12 @@
+import { validateRequest } from "@/lib/auth";
 import NewProjectForm from "./_parts/new-project-form";
 
-export default function Page() {
+export default async function Page() {
+  const { user } = await validateRequest();
+
   return (
     <main className="container space-y-10 py-3">
-      <NewProjectForm />
+      <NewProjectForm username={user!.username} userId={user!.id} />
     </main>
   );
 }
