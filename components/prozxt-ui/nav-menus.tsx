836b6b6b071fe1255fce74def59fe39b996ac605
Rifat Khan <rifat6633@gmail.com>
2024-04-17 22:51:32 +0600

    fix: Projects menu title

--- components/prozxt-ui/nav-menus.tsx
+++ components/prozxt-ui/nav-menus.tsx
@@ -43,7 +43,7 @@ export function UserProfileSidebar({
         icon: TbPencil,
       },
       {
-        title: "Profile",
+        title: "Projects",
         href: `/${username}/projects`,
         icon: TbSettings,
       },
