bb54e68dc100ac60934f8fff5eee92a5e92b81a4
Rifat Khan <rifat6633@gmail.com>
2024-04-17 22:16:15 +0600

    add: functionality for saving project in db and return response

--- app/(new-edit)/new/project/_parts/actions.ts
+++ app/(new-edit)/new/project/_parts/actions.ts
@@ -1,16 +1,45 @@
 "use server";
 
+import prisma from "@/lib/prismadb";
 import { SerializedEditorState, SerializedLexicalNode } from "lexical";
 
 type NewProjectData = {
+  userId: string;
   title: string;
   tags: string[];
   editorState: SerializedEditorState<SerializedLexicalNode> | undefined;
 };
 
 export async function addNewProject(data: NewProjectData) {
+  const { userId, title, editorState, tags } = data;
+
   try {
     console.log(data);
+    const project = await prisma.project.create({
+      data: {
+        title: title,
+        body: JSON.stringify(editorState),
+        tags: JSON.stringify(tags),
+        user: {
+          connect: {
+            id: userId,
+          },
+        },
+      },
+    });
+
+    if (project) {
+      return {
+        projectId: project.id,
+        success: true,
+        message: "Project is published!",
+      };
+    } else {
+      return {
+        success: false,
+        message: "Failed to save!",
+      };
+    }
   } catch (err) {
     console.log(err);
     throw err;
--- app/(new-edit)/new/project/_parts/new-project-form.tsx
+++ app/(new-edit)/new/project/_parts/new-project-form.tsx
@@ -9,24 +9,54 @@ import { FormEvent, useState } from "react";
 import { addNewProject } from "./actions";
 import { Badge } from "@/components/ui/badge";
 import { RemoveXBtn } from "@/components/prozxt-ui/button";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
+import { Separator } from "@/components/ui/separator";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 type FormData = {
   tags: string;
 };
 
-export default function NewProjectForm() {
+export default function NewProjectForm({
+  username,
+  userId,
+}: {
+  username: string;
+  userId: string;
+}) {
   const [title, setTitle] = useState("");
   const [formData, setFormData] = useState<FormData>({ tags: "" });
   const [tags, setTags] = useState<string[]>([]);
   const [editorState, setEditorState] = useState<EditorState>();
+  const router = useRouter();
 
-  function handleClick() {
+  async function handleClick() {
     const data = {
+      userId,
       title,
       tags,
       editorState: editorState?.toJSON(),
     };
-    addNewProject(data);
+    if (
+      title.length !== 0 &&
+      !!editorState?.toJSON().root.children &&
+      editorState?.toJSON().root.children.length >= 1 &&
+      // @ts-ignore: Unreachable code error
+      editorState?.toJSON().root.children[0].children.length > 0
+    ) {
+      console.log(data);
+      const res = await addNewProject(data);
+
+      if (res.success) {
+        toast.success(res.message);
+        router.push(`/${username}/projects/${res.projectId}`);
+      } else {
+        toast.error(res.message);
+      }
+    } else {
+      toast.error("Write something First!");
+    }
   }
 
   function hangleTagSubmit(e: FormEvent) {
@@ -51,8 +81,18 @@ export default function NewProjectForm() {
         </Button>
       </section>
 
+      {/* Project Description */}
+      <ScrollArea className="h-96 w-full">
+        <LexicalEditor
+          placeholder="Project description..."
+          setEditorState={setEditorState}
+        />
+      </ScrollArea>
+
+      <Separator />
+
       {/* Project Tags */}
-      <section className="relative flex items-center gap-4">
+      <section className="relative flex flex-wrap items-center gap-4">
         {tags.length !== 0 &&
           tags.map((tag, i) => (
             <div key={i} className="relative">
@@ -68,30 +108,19 @@ export default function NewProjectForm() {
             </div>
           ))}
 
-        {formData.tags === "" && tags.length === 0 && (
-          <div className="absolute inset-0 -z-20 text-muted-foreground/50">
-            Tags...
-          </div>
-        )}
-
         <form onSubmit={(e) => hangleTagSubmit(e)} className="w-full flex-1">
           <TagInput
             name="tags"
             value={formData.tags}
+            placeholder="Add tags..."
             onChange={(e) => setFormData({ ...formData, tags: e.target.value })}
-            className=""
+            className="w-full"
           />
           <Button type="submit" className="sr-only">
             Add Tag
           </Button>
         </form>
       </section>
-
-      {/* Project Description */}
-      <LexicalEditor
-        placeholder="Project description..."
-        setEditorState={setEditorState}
-      />
     </section>
   );
 }
