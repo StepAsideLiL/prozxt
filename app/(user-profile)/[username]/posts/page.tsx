ca8454cfcfe4d9dae0d5607dc005a7fe49af5a32
Rifat Khan <rifat6633@gmail.com>
2024-04-18 19:06:31 +0600

    add: UserPosts with username prop

--- app/(user-profile)/[username]/posts/page.tsx
+++ app/(user-profile)/[username]/posts/page.tsx
@@ -1,3 +1,12 @@
-export default function Page() {
-  return <main>Posts</main>;
+import { Suspense } from "react";
+import UserPosts from "./_parts/user-posts";
+
+export default function Page({ params }: { params: { username: string } }) {
+  return (
+    <main className="container space-y-10 py-3">
+      <Suspense fallback={"loading"}>
+        <UserPosts username={params.username} />
+      </Suspense>
+    </main>
+  );
 }
