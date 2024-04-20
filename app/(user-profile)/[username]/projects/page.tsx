5db1009b950069a3014507f34060d99c8f99e8bf
Rifat Khan <rifat6633@gmail.com>
2024-04-20 12:43:16 +0600

    add: UserProjects to list all the projects

--- app/(user-profile)/[username]/projects/page.tsx
+++ app/(user-profile)/[username]/projects/page.tsx
@@ -1,3 +1,12 @@
-export default function Page() {
-  return <main>Projects</main>;
+import { Suspense } from "react";
+import UserProjects from "./_parts/user-projects";
+
+export default function Page({ params }: { params: { username: string } }) {
+  return (
+    <main className="container space-y-10 py-3">
+      <Suspense fallback={"loading"}>
+        <UserProjects username={params.username} />
+      </Suspense>
+    </main>
+  );
 }
