e0b64e2c24583c43fe72a0e865fe43f0b2c4f592
Rifat Khan <rifat6633@gmail.com>
2024-04-20 17:14:43 +0600

    add: UserPost

--- app/(user-profile)/[username]/posts/[post]/page.tsx
+++ app/(user-profile)/[username]/posts/[post]/page.tsx
@@ -1,11 +1,16 @@
+import { Suspense } from "react";
+import UserPost from "./_parts/user-post";
+
 export default function Page({
   params,
 }: {
   params: { username: string; post: string };
 }) {
   return (
-    <main>
-      {params.username} {params.post}
+    <main className="container space-y-10 py-3">
+      <Suspense fallback={"loading"}>
+        <UserPost username={params.username} postId={params.post} />
+      </Suspense>
     </main>
   );
 }
