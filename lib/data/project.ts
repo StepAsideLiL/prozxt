2d4f4ca7f2540e43facb514ba153a6481144d223
Rifat Khan <rifat6633@gmail.com>
2024-04-20 17:23:06 +0600

    create: getProjctById to fetch project data by project id

--- lib/data/project.ts
+++ lib/data/project.ts
@@ -39,3 +39,39 @@ export async function getUserProjectsByUsername(username: string) {
     throw new Error("Failed to fetch user projects.");
   }
 }
+
+export async function getProjctById(projectId: string) {
+  try {
+    const project = await prisma.project.findUnique({
+      where: {
+        id: projectId,
+      },
+      select: {
+        id: true,
+        title: true,
+        body: true,
+        tags: true,
+        createdAt: true,
+        updatedAt: true,
+        user: {
+          select: {
+            id: true,
+            username: true,
+            profilePicture: {
+              select: {
+                width: true,
+                height: true,
+                url: true,
+              },
+            },
+          },
+        },
+      },
+    });
+
+    return project;
+  } catch (err) {
+    console.log(err);
+    throw new Error("Failed to to fetch project.");
+  }
+}
