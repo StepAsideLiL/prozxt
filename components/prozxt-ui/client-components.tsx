1996a99ac1d8c5d76ec13f1fde3e8d9955e67210
Rifat Khan <rifat6633@gmail.com>
2024-04-20 13:30:11 +0600

    update: Link subpage

--- components/prozxt-ui/client-components.tsx
+++ components/prozxt-ui/client-components.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { RxSlash } from "react-icons/rx";
 
@@ -16,9 +17,9 @@ export default function ProfileSubPage() {
     <div className="flex items-center gap-3">
       <RxSlash size={20} className="text-muted-foreground" />
 
-      <p className="font-medium">
+      <Link href={paths.slice(0, 3).join("/")} className="font-medium">
         {pagename[0].toUpperCase() + pagename.slice(1)}
-      </p>
+      </Link>
     </div>
   );
 }
