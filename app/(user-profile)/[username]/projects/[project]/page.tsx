7d59b01d27b289871f4a65d0f7932bee476237dd
Rifat Khan <rifat6633@gmail.com>
2024-04-20 17:17:51 +0600

    add: UserProject

--- app/(user-profile)/[username]/projects/[project]/page.tsx
+++ app/(user-profile)/[username]/projects/[project]/page.tsx
@@ -1,11 +1,16 @@
+import { Suspense } from "react";
+import UserProject from "./_parts/user-project";
+
 export default function Page({
   params,
 }: {
   params: { username: string; project: string };
 }) {
   return (
-    <main>
-      {params.username} {params.project}
+    <main className="container space-y-10 py-3">
+      <Suspense fallback={"loading"}>
+        <UserProject username={params.username} projectId={params.project} />
+      </Suspense>
     </main>
   );
 }
