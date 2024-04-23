b1a77bca24a2b35937b5be674a222871c19b4dba
Rifat Khan <rifat6633@gmail.com>
2024-04-23 12:04:55 +0600

    update: redirect user to sign-in page if the user tries to edit other's portfolio

--- app/(user-profile)/[username]/portfolio/edit/page.tsx
+++ app/(user-profile)/[username]/portfolio/edit/page.tsx
@@ -1,16 +1,26 @@
 import { getUserPortfolioByUsername } from "@/lib/data/portfolio";
 import EditPortfolioForm from "./_parts/edit-portfolio-form";
+import { validateRequest } from "@/lib/auth";
+import { redirect } from "next/navigation";
 
 export default async function Page({
   params,
 }: {
   params: { username: string };
 }) {
-  const user = await getUserPortfolioByUsername(params.username);
+  const { user } = await validateRequest();
+  const userPortfolio = await getUserPortfolioByUsername(params.username);
+
+  if (user?.username !== params.username) {
+    redirect("/auth/sign-in");
+  }
 
   return (
     <main className="container space-y-10 py-3">
-      <EditPortfolioForm userId={user!.id} content={user?.portfolio?.body} />
+      <EditPortfolioForm
+        userId={user!.id}
+        content={userPortfolio?.portfolio?.body}
+      />
     </main>
   );
 }
