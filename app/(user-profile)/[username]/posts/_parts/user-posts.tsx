c0b03f5fec5c1d3e83ee0a1436a9f5d280f371d3
Rifat Khan <rifat6633@gmail.com>
2024-04-23 14:02:56 +0600

    add: new post button

--- app/(user-profile)/[username]/posts/_parts/user-posts.tsx
+++ app/(user-profile)/[username]/posts/_parts/user-posts.tsx
@@ -3,12 +3,25 @@ import { getUserPostsByUsername } from "@/lib/data/post";
 import Link from "next/link";
 import { format, formatDistanceToNow } from "date-fns";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { validateRequest } from "@/lib/auth";
+import { Plus } from "lucide-react";
 
 export default async function UserPosts({ username }: { username: string }) {
+  const { user } = await validateRequest();
   const posts = await getUserPostsByUsername(username);
 
   return (
-    <section className="mx-auto max-w-3xl">
+    <section className="mx-auto max-w-3xl space-y-3">
+      {user?.username === username && posts.length !== 0 && (
+        <>
+          <Button variant={"outline"} asChild>
+            <Link href={`/new/post`} className="gap-1">
+              <Plus size={16} /> New Post
+            </Link>
+          </Button>
+        </>
+      )}
+
       {posts.length !== 0 ? (
         <section className="space-y-3">
           {posts.map((post) => (
@@ -57,7 +70,7 @@ export default async function UserPosts({ username }: { username: string }) {
           ))}
         </section>
       ) : (
-        <div className="py-20">
+        <div className="flex flex-col items-center justify-center gap-2 py-20">
           <h1 className="text-center text-2xl text-muted-foreground">
             No Post Found
           </h1>
