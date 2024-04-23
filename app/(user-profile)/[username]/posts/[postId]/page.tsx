a569e5487cb47d6a8cd44438def8285724a7948a
Rifat Khan <rifat6633@gmail.com>
2024-04-23 12:51:34 +0600

    create: UserPostSkeleton

--- app/(user-profile)/[username]/posts/[postId]/page.tsx
+++ app/(user-profile)/[username]/posts/[postId]/page.tsx
@@ -1,5 +1,6 @@
 import { Suspense } from "react";
 import UserPost from "./_parts/user-post";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export default function Page({
   params,
@@ -8,9 +9,35 @@ export default function Page({
 }) {
   return (
     <main className="container space-y-10 py-3">
-      <Suspense fallback={"loading"}>
+      <Suspense fallback={<UserPostSkeleton />}>
         <UserPost username={params.username} postId={params.postId} />
       </Suspense>
     </main>
+  );
+}
+
+function UserPostSkeleton() {
+  return (
+    <article className="mx-auto max-w-3xl space-y-4">
+      <Skeleton className="h-9 w-full" />
+
+      <section className="flex items-start gap-5">
+        <div className="flex items-start gap-5">
+          <Skeleton className="h-10 w-10 rounded-full" />
+
+          <div className="space-y-2">
+            <Skeleton className="h-4 w-16" />
+            <Skeleton className="h-3 w-24" />
+          </div>
+        </div>
+      </section>
+
+      <div className="space-y-2">
+        <Skeleton className="h-5 w-full" />
+        <Skeleton className="h-5 w-full" />
+        <Skeleton className="h-5 w-full" />
+        <Skeleton className="h-5 w-full" />
+      </div>
+    </article>
   );
 }
