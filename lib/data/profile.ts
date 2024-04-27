d646b59f5e0ebab0583766258ac39300c374ce6c
Rifat Khan <rifat6633@gmail.com>
2024-04-27 16:09:15 +0600

    create: pinProjectByUserIdAndProjectId server action

--- lib/data/profile.ts
+++ lib/data/profile.ts
@@ -45,3 +45,50 @@ export async function pinPostByUserIdAndPostId(userId: string, postId: string) {
     throw new Error("Failed to perform pinPostByUserIdAndPostId");
   }
 }
+
+export async function pinProjectByUserIdAndProjectId(
+  userId: string,
+  projectId: string,
+) {
+  try {
+    const pinProject = await prisma.profile.upsert({
+      where: {
+        userId: userId,
+      },
+      update: {
+        pinProject: {
+          connect: {
+            id: projectId,
+          },
+        },
+      },
+      create: {
+        user: {
+          connect: {
+            id: userId,
+          },
+        },
+        pinProject: {
+          connect: {
+            id: projectId,
+          },
+        },
+      },
+    });
+
+    if (pinProject) {
+      return {
+        success: true,
+        message: "The Post is Pinned!",
+      };
+    } else {
+      return {
+        success: false,
+        message: "Failed to pin!",
+      };
+    }
+  } catch (err) {
+    console.log(err);
+    throw new Error("Failed to perform pinProjectByUserIdAndProjectId");
+  }
+}
