24fc9f9b653df96247290825dd7f6618d0809d9b
Rifat Khan <rifat6633@gmail.com>
2024-04-27 09:22:45 +0600

    update: hide edit button for invalid post and wrap project content in article tag

--- app/(user-profile)/[username]/projects/[projectId]/_parts/user-project.tsx
+++ app/(user-profile)/[username]/projects/[projectId]/_parts/user-project.tsx
@@ -18,50 +18,55 @@ export default async function UserProject({
   const project = await getProjctById(projectId);
 
   return (
-    <section className="mx-auto max-w-3xl space-y-6">
-      {user?.username === username && (
-        <>
-          <Button variant={"outline"} asChild>
-            <Link
-              href={`/${username}/projects/${projectId}/edit`}
-              className="gap-1"
-            >
-              <Edit size={16} /> Edit
-            </Link>
-          </Button>
-        </>
-      )}
-
+    <section className="mx-auto max-w-3xl">
       {project ? (
         <>
-          <div className="space-y-4">
-            <h1 className="text-4xl font-bold">{project.title}</h1>
-
-            <section className="flex items-start gap-5">
-              <Link href={`/${project.user.username}`}>
-                <Avatar>
-                  <AvatarImage src={project.user.profilePicture?.url} />
-                  <AvatarFallback>
-                    {project.user.username[0].toUpperCase()}
-                  </AvatarFallback>
-                </Avatar>
-              </Link>
-
-              <div>
-                <h1>
-                  <Link href={`/${project.user.username}`}>
-                    {project.user.username}
+          <section className="space-y-6">
+            {user?.username === username && (
+              <>
+                <Button variant={"outline"} asChild>
+                  <Link
+                    href={`/${username}/projects/${projectId}/edit`}
+                    className="gap-1"
+                  >
+                    <Edit size={16} /> Edit
                   </Link>
-                </h1>
-                <p className="text-xs text-muted-foreground">
-                  {format(new Date(project.createdAt), `dd MMMM yyyy`)} (Posted{" "}
-                  {formatDistanceToNow(new Date(project.createdAt))} ago)
-                </p>
-              </div>
-            </section>
-          </div>
+                </Button>
+              </>
+            )}
 
-          <LexicalReadonly content={project.body} />
+            <article className="space-y-6">
+              <div className="space-y-4">
+                <h1 className="text-4xl font-bold">{project.title}</h1>
+
+                <section className="flex items-start gap-5">
+                  <Link href={`/${project.user.username}`}>
+                    <Avatar>
+                      <AvatarImage src={project.user.profilePicture?.url} />
+                      <AvatarFallback>
+                        {project.user.username[0].toUpperCase()}
+                      </AvatarFallback>
+                    </Avatar>
+                  </Link>
+
+                  <div>
+                    <h1>
+                      <Link href={`/${project.user.username}`}>
+                        {project.user.username}
+                      </Link>
+                    </h1>
+                    <p className="text-xs text-muted-foreground">
+                      {format(new Date(project.createdAt), `dd MMMM yyyy`)}{" "}
+                      (Posted {formatDistanceToNow(new Date(project.createdAt))}{" "}
+                      ago)
+                    </p>
+                  </div>
+                </section>
+              </div>
+
+              <LexicalReadonly content={project.body} />
+            </article>
+          </section>
         </>
       ) : (
         <>
