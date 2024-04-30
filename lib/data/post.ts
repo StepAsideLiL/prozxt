92c803f0355a5b418eff7091205ec769a66de0c4
Rifat Khan <rifat6633@gmail.com>
2024-04-30 11:05:58 +0600

    create: getHomePosts server action

--- lib/data/post.ts
+++ lib/data/post.ts
@@ -99,3 +99,39 @@ export async function deletePostById(postId: string) {
     throw new Error("Failed to perform deletePostById function.");
   }
 }
+
+export async function getHomePosts() {
+  try {
+    const posts = await prisma.post.findMany({
+      take: 10,
+      orderBy: {
+        createdAt: "desc",
+      },
+      select: {
+        id: true,
+        title: true,
+        body: true,
+        createdAt: true,
+        updatedAt: true,
+        user: {
+          select: {
+            id: true,
+            username: true,
+            profilePicture: {
+              select: {
+                width: true,
+                height: true,
+                url: true,
+              },
+            },
+          },
+        },
+      },
+    });
+
+    return posts;
+  } catch (err) {
+    console.log(err);
+    throw new Error("Failed to perform getHomePosts.");
+  }
+}
