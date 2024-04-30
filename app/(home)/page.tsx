8cbe0171e3e2c21ac6aa90f011cd21401c7bcead
Rifat Khan <rifat6633@gmail.com>
2024-04-30 11:09:01 +0600

    create: PostsSkeleton component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,5 +1,6 @@
 import { Suspense } from "react";
 import Posts from "./_parts/posts";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export default function Page() {
   return (
@@ -12,5 +13,26 @@ export default function Page() {
 }
 
 function PostsSkeleton() {
-  return <section className="mx-auto max-w-3xl space-y-3">loading</section>;
+  return (
+    <section className="mx-auto max-w-3xl space-y-3">
+      <article className="space-y-3 rounded bg-muted/25 p-5">
+        <div className="flex items-start gap-5">
+          <Skeleton className="h-10 w-10 rounded-full" />
+
+          <div className="space-y-2">
+            <Skeleton className="h-4 w-16" />
+            <Skeleton className="h-3 w-24" />
+          </div>
+        </div>
+
+        <div className="flex items-start gap-5">
+          <div className="w-10"></div>
+
+          <div>
+            <Skeleton className="h-6 w-full" />
+          </div>
+        </div>
+      </article>
+    </section>
+  );
 }
