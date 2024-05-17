625a07ab5a8088d388a53492fd720ad1b6861abd
Rifat Khan <rifat6633@gmail.com>
2024-05-17 21:00:42 +0600

    design: post page

--- app/(home)/post/page.tsx
+++ app/(home)/post/page.tsx
@@ -1,3 +1,43 @@
+import { Suspense } from "react";
+import Posts from "./_parts/posts";
+import { Skeleton } from "@/components/ui/skeleton";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Post",
+};
+
 export default function Page() {
-  return <main>Post Page</main>;
+  return (
+    <main className="container py-3">
+      <Suspense fallback={<PostsSkeleton />}>
+        <Posts />
+      </Suspense>
+    </main>
+  );
+}
+
+function PostsSkeleton() {
+  return (
+    <section className="mx-auto max-w-3xl space-y-3">
+      <article className="space-y-3 rounded bg-muted/25 p-5">
+        <div className="flex items-start gap-5">
+          <Skeleton className="h-10 w-10 rounded-full" />
+
+          <div className="space-y-2">
+            <Skeleton className="h-4 w-16" />
+            <Skeleton className="h-3 w-24" />
+          </div>
+        </div>
+
+        <div className="flex items-start gap-5">
+          <div className="w-10"></div>
+
+          <div>
+            <Skeleton className="h-6 w-full" />
+          </div>
+        </div>
+      </article>
+    </section>
+  );
 }
