0103e4d7ebaa4d9f33b30da2c9d98feeb691fc81
Rifat Khan <rifat6633@gmail.com>
2024-05-17 21:02:04 +0600

    update: home page redirect to /post page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,38 +1,5 @@
-import { Suspense } from "react";
-import Posts from "./_parts/posts";
-import { Skeleton } from "@/components/ui/skeleton";
+import { redirect } from "next/navigation";
 
 export default function Page() {
-  return (
-    <main className="container py-3">
-      <Suspense fallback={<PostsSkeleton />}>
-        <Posts />
-      </Suspense>
-    </main>
-  );
-}
-
-function PostsSkeleton() {
-  return (
-    <section className="mx-auto max-w-3xl space-y-3">
-      <article className="space-y-3 rounded bg-muted/25 p-5">
-        <div className="flex items-start gap-5">
-          <Skeleton className="h-10 w-10 rounded-full" />
-
-          <div className="space-y-2">
-            <Skeleton className="h-4 w-16" />
-            <Skeleton className="h-3 w-24" />
-          </div>
-        </div>
-
-        <div className="flex items-start gap-5">
-          <div className="w-10"></div>
-
-          <div>
-            <Skeleton className="h-6 w-full" />
-          </div>
-        </div>
-      </article>
-    </section>
-  );
+  redirect("/post");
 }
