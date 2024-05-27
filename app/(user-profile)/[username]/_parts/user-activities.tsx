ce52d4e9fe1c7aad89212a694a617454a03d6fa4
Rifat Khan <rifat6633@gmail.com>
2024-05-27 23:21:47 +0600

    add Portfolio card and redesign card

--- app/(user-profile)/[username]/_parts/user-activities.tsx
+++ app/(user-profile)/[username]/_parts/user-activities.tsx
@@ -1,3 +1,4 @@
+import { Badge } from "@/components/ui/badge";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { getUserActivities } from "@/lib/data/user";
 import Link from "next/link";
@@ -11,14 +12,17 @@ export default async function UserActivities({
 
   return (
     <section className="mx-auto flex max-w-3xl gap-5">
+      <Link href={`/${username}/portfolio`} className="w-full">
+        <Card>
+          <CardContent className="p-3 font-medium">Your Portfolio</CardContent>
+        </Card>
+      </Link>
       {activities.map((activity) => (
         <Link href={activity.href} key={activity.title} className="w-full">
           <Card>
-            <CardHeader>
-              <CardTitle>{activity.title}</CardTitle>
-            </CardHeader>
-            <CardContent>
-              <h3 className="text-2xl font-semibold">{activity.count}</h3>
+            <CardContent className="flex items-center gap-5 p-3 font-medium">
+              <p>Your {activity.title}</p>
+              <Badge variant={"outline"}>{activity.count}</Badge>
             </CardContent>
           </Card>
         </Link>
