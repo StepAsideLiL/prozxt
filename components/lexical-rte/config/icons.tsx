22e360658fc5313250550f6030e68207a9497430
Rifat Khan <rifat6633@gmail.com>
2024-06-01 10:30:55 +0600

    create icon object

---
+++ components/lexical-rte/config/icons.tsx
@@ -0,0 +1,19 @@
+import {
+  Heading1,
+  Heading2,
+  Heading3,
+  List,
+  ListOrdered,
+  ListTodo,
+  Pilcrow,
+} from "lucide-react";
+
+export const icon = {
+  paragraph: Pilcrow,
+  h1: Heading1,
+  h2: Heading2,
+  h3: Heading3,
+  unorderedList: List,
+  orderedList: ListOrdered,
+  checkList: ListTodo,
+};
