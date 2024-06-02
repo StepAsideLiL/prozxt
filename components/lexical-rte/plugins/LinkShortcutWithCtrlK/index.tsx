87a2709c5fedde46655ac0dc265179f64c9f65b0
Rifat Khan <rifat6633@gmail.com>
2024-06-03 00:05:16 +0600

    create LinkShortcutWithCtrlK

---
+++ components/lexical-rte/plugins/LinkShortcutWithCtrlK/index.tsx
@@ -0,0 +1,85 @@
+/**
+ * This plugin toggles selection between a link and a text node with Ctrl+K.
+ */
+
+import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
+import {
+  $getSelection,
+  $isRangeSelection,
+  COMMAND_PRIORITY_CRITICAL,
+  COMMAND_PRIORITY_NORMAL,
+  KEY_MODIFIER_COMMAND,
+  SELECTION_CHANGE_COMMAND,
+} from "lexical";
+import { Dispatch, useCallback, useEffect, useState } from "react";
+import { sanitizeUrl } from "../../utils/url";
+import { $isLinkNode, TOGGLE_LINK_COMMAND } from "@lexical/link";
+import { getSelectedNode } from "../../utils/getSelectedNode";
+
+export default function LinkShortcutWithCtrlK({
+  setIsLinkEditMode,
+}: {
+  setIsLinkEditMode: Dispatch<boolean>;
+}) {
+  const [editor] = useLexicalComposerContext();
+
+  // Editor states.
+  const [activeEditor, setActiveEditor] = useState(editor);
+  const [isLink, setIsLink] = useState(false);
+
+  // Update the state of the toolbar
+  const $updateToolbar = useCallback(() => {
+    const selection = $getSelection();
+
+    if ($isRangeSelection(selection)) {
+      const node = getSelectedNode(selection);
+      const parent = node.getParent();
+
+      // Update links
+      if ($isLinkNode(parent) || $isLinkNode(node)) {
+        setIsLink(true);
+      } else {
+        setIsLink(false);
+      }
+    }
+  }, []);
+
+  useEffect(() => {
+    return editor.registerCommand(
+      SELECTION_CHANGE_COMMAND,
+      (_payload, newEditor) => {
+        $updateToolbar();
+        setActiveEditor(newEditor);
+        return false;
+      },
+      COMMAND_PRIORITY_CRITICAL,
+    );
+  }, [editor, $updateToolbar]);
+
+  useEffect(() => {
+    return activeEditor.registerCommand(
+      KEY_MODIFIER_COMMAND,
+      (payload) => {
+        const event: KeyboardEvent = payload;
+        const { code, ctrlKey, metaKey } = event;
+
+        if (code === "KeyK" && (ctrlKey || metaKey)) {
+          event.preventDefault();
+          let url: string | null;
+          if (!isLink) {
+            setIsLinkEditMode(true);
+            url = sanitizeUrl("https://");
+          } else {
+            setIsLinkEditMode(false);
+            url = null;
+          }
+          return activeEditor.dispatchCommand(TOGGLE_LINK_COMMAND, url);
+        }
+        return false;
+      },
+      COMMAND_PRIORITY_NORMAL,
+    );
+  }, [activeEditor, isLink, setIsLinkEditMode]);
+
+  return null;
+}
